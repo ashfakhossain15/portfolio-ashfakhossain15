de42895edac9851c8580f35d43ae316631b48c57
Ashfak Hossain <ashfaksikder15@gmail.com>
2023-12-09 22:35:04 +0600

    update skills part

--- src/Components/Pages/Skills/Skills.jsx
+++ src/Components/Pages/Skills/Skills.jsx
@@ -1,8 +1,23 @@
+import { IoLogoHtml5, IoLogoCss3, IoLogoReact } from "react-icons/io5";
+import { FaBootstrap } from "react-icons/fa6";
+import {
+  SiTailwindcss,
+  SiMongodb,
+  SiDaisyui,
+  SiTypescript,
+  SiNextdotjs,
+  SiExpress,
+  SiRedux,
+  SiReactrouter,
+} from "react-icons/si";
+import { FaSass } from "react-icons/fa";
+
+// SiCss3, FaReact, FaBootstrap
 const Skills = () => {
   return (
     <div>
       <div className="skills " id="skills">
-        <h2 className="heading relative ">
+        <h2 className="heading relative mb-8 ">
           My <span>Skills</span>
           <span
             className="animate about-head absolute "
@@ -11,83 +26,123 @@ const Skills = () => {
         </h2>
         <div className="skills-row">
           <div className="skills-colum">
-            <h3 className="title relative">
-              Coding Skills{" "}
-              <span
-                className="animate about-head absolute "
-                style={{ "--i": "2" }}
-              ></span>
-            </h3>
-            <div className="skills-box relative">
-              <div className="skills-content">
-                <div className="progres">
-                  <h3>
-                    HTML <span>95%</span>
-                  </h3>
-                  <div className="bar">
-                    {" "}
-                    <span className="w-[95%]"></span>
-                  </div>
-                </div>
-                <div className="progres">
-                  <h3>
-                    CSS <span>82%</span>
-                  </h3>
-                  <div className="bar">
-                    <span className="w-[82%]"></span>
-                  </div>
-                </div>
-                <div className="progres">
-                  <h3>
-                    Javascript<span>65%</span>
-                  </h3>
-                  <div className="bar">
-                    <span className="w-[65%]"></span>
-                  </div>
-                </div>
+            <div className="skills-box relative grid grid-cols-2 sm:gird-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 text-center">
+              <div
+                className="gap-2 py-3 px-10 flex items-center rounded-md"
+                style={{ border: "1px solid #00abf0" }}
+              >
+                <IoLogoHtml5 size={30} />
+                HTML
               </div>
-              <span
-                className="animate about-head absolute "
-                style={{ "--i": "3" }}
-              ></span>
-            </div>
-          </div>
-          <div className="skills-colum">
-            <h3 className="title relative">
-              Professional Skills{" "}
-              <span
-                className="animate about-head absolute "
-                style={{ "--i": "5" }}
-              ></span>
-            </h3>
-            <div className="skills-box relative">
-              <div className="skills-content">
-                <div className="progres">
-                  <h3>
-                    Web Design <span>80%</span>
-                  </h3>
-                  <div className="bar">
-                    {" "}
-                    <span className="w-[80%]"></span>
-                  </div>
-                </div>
-                <div className="progres">
-                  <h3>
-                    Web Development <span>85%</span>
-                  </h3>
-                  <div className="bar">
-                    <span className="w-[85%]"></span>
-                  </div>
-                </div>
-                <div className="progres">
-                  <h3>
-                    Authentication<span>65%</span>
-                  </h3>
-                  <div className="bar">
-                    <span className="w-[65%]"></span>
-                  </div>
-                </div>
+              <div
+                className="gap-2 py-3 px-10 flex items-center rounded-md"
+                style={{ border: "1px solid #00abf0" }}
+              >
+                <IoLogoCss3 size={30} />
+                CSS
               </div>
+              <div
+                className="gap-2 py-3 px-10 flex items-center rounded-md"
+                style={{ border: "1px solid #00abf0" }}
+              >
+                {/* <h1 className="text-5xl"> */}
+                <SiTailwindcss size={30} />
+                {/* </h1> */}
+                Tailwined
+              </div>
+              <div
+                className="gap-2 py-3 px-10 flex items-center rounded-md"
+                style={{ border: "1px solid #00abf0" }}
+              >
+                <FaBootstrap size={30} />
+                Bootstrap
+              </div>
+              <div
+                className="gap-2 py-3 px-10 flex items-center rounded-md"
+                style={{ border: "1px solid #00abf0" }}
+              >
+                <IoLogoReact size={30} />
+                React Js
+              </div>
+              <div
+                className="gap-2 py-3 px-10 flex items-center rounded-md"
+                style={{ border: "1px solid #00abf0" }}
+              >
+                <FaSass size={30} />
+                SASS
+              </div>
+              <div
+                className="gap-2 py-3 px-10 flex items-center rounded-md"
+                style={{ border: "1px solid #00abf0" }}
+              >
+                <SiDaisyui oReact size={30} />
+                DaisyUI
+              </div>
+              <div
+                className="gap-2 py-3 px-10 flex items-center rounded-md"
+                style={{ border: "1px solid #00abf0" }}
+              >
+                <SiTypescript size={30} />
+                TypeScript
+              </div>
+
+              <div
+                className="gap-2 py-3 px-10 flex items-center rounded-md"
+                style={{ border: "1px solid #00abf0" }}
+              >
+                <SiNextdotjs size={30} />
+                Next.Js
+              </div>
+              <div
+                className="gap-2 py-3 px-10 flex items-center rounded-md"
+                style={{ border: "1px solid #00abf0" }}
+              >
+                <SiReactrouter size={30} />
+                React Router
+              </div>
+              <div
+                className="gap-2 py-3 px-10 flex items-center rounded-md"
+                style={{ border: "1px solid #00abf0" }}
+              >
+                <SiRedux size={30} />
+                Redux
+              </div>
+              <div
+                className="gap-2 py-3 px-10 flex items-center rounded-md"
+                style={{ border: "1px solid #00abf0" }}
+              >
+                <IoLogoReact size={30} />
+                Recharts
+              </div>
+              <div
+                className="gap-2 py-3 px-10 flex items-center rounded-md"
+                style={{ border: "1px solid #00abf0" }}
+              >
+                <SiExpress size={30} />
+                Express.Js
+              </div>
+              <div
+                className="gap-2 py-3 px-10 flex items-center rounded-md"
+                style={{ border: "1px solid #00abf0" }}
+              >
+                <IoLogoReact size={30} />
+                JSON Web Tokens
+              </div>
+              <div
+                className="gap-2 py-3 px-10 flex items-center rounded-md"
+                style={{ border: "1px solid #00abf0" }}
+              >
+                <IoLogoReact size={30} />
+                RESTful APIs
+              </div>
+              <div
+                className="gap-2 py-3 px-10 flex items-center rounded-md"
+                style={{ border: "1px solid #00abf0" }}
+              >
+                <SiMongodb size={30} />
+                MongoDB
+              </div>
+
               <span
                 className="animate about-head absolute "
                 style={{ "--i": "6" }}
